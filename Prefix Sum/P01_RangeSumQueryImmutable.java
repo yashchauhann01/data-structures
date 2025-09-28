f106eae6c137bf4bbd91d9f68b633c57ff973465
Yash Chauhan <chauhan19yash@gmail.com>
2025-09-28 22:49:09 +0530

    Update P01_RangeSumQueryImmutable.java

--- Prefix Sum/P01_RangeSumQueryImmutable.java
+++ Prefix Sum/P01_RangeSumQueryImmutable.java
@@ -1,2 +1,10 @@
-### LeetCode Problem Link: [303. Range Sum Query - Immutable](https://leetcode.com/problems/range-sum-query-immutable/description/)
+### LeetCode Problem Link: [303. Range Sum Query - Immutable](https://leetcode.com/problems/range-sum-query-immutable/description)
+```java
+class Solution{
+  ListNode nod = new ListNode(0);
+  
+}
+  ```                                                   
                                                               
+                                                              
+
